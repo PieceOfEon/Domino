3c6485e4b1a2009348efcca5bd40f68fc2f26209
PieceOfEon <danilskripka1996@gmail.com>
2022-12-13 14:57:54 +0200

    addBoneADD

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,12 +23,13 @@ namespace Domino
 
         List<Bone> bon = new List<Bone>();
         int a = 0;
+        int kol = 0;
         public MainWindow()
         {
             
             InitializeComponent();
 
-            five_six.MouseLeftButtonDown += five_six_MouseLeftButtonDown;
+            //five_six.MouseLeftButtonDown += five_six_MouseLeftButtonDown;
             //third.BindingGroup.Items.Add(elem0);
             //MessageBox.Show(Convert.ToString(elem0));
 
@@ -80,13 +81,84 @@ namespace Domino
             five_six.RenderTransform = new RotateTransform(a);
         }
 
+        //private void five_six_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if(e.ButtonState==MouseButtonState.Pressed)
+        //    {
+        //        Left = e.GetPosition(this).X;
+        //        Top = e.GetPosition(this).Y;
+        //    }
+        //    //five_six.RenderTransform = new MatrixTransform(0, 0,0,0, e.GetPosition(this).X, e.GetPosition(this).Y);
+        //    //if (e.LeftButton == MouseButtonState.Pressed)
+        //    //{
+        //    //    Left =e.GetPosition(this).X;
+        //    //    Top = e.GetPosition(this).Y;
+        //    //    //MouseEventArgs mea = new MouseEventArgs(MouseDevice mouse, int timestamp, StylusDevice stylusDevice);
+        //    //    //Point p = e.GetPosition(this);
+        //    //    //MessageBox.Show("Margin=" + five_six.Margin.ToString());
+        //    //    //five_six.Margin = new Thickness(p.X, 0, 0, p.Y);
+        //    //    //MessageBox.Show("PX=" + p.X.ToString(), "PY=" + p.Y.ToString());
+        //    //    //MessageBox.Show("Margin=" + five_six.Margin.ToString());
+        //    //    //MessageBox.Show(e.GetPosition(this).X.ToString(), e.GetPosition(this).Y.ToString());
+        //    //}
+        //}
+
+        private int m_dragIndx;
+        bool m_inMouseMove;
+
+        private void five_six_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            //if (!m_inMouseMove)
+            //{
+            //    m_inMouseMove = true;
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                ++m_dragIndx;
+
+                DragDrop.DoDragDrop(five_six, m_dragIndx, DragDropEffects.All);
+                e.Handled = true;
+                if (e.LeftButton==MouseButtonState.Released)
+                {
+
+                    ALBE.Content = five_six.Margin.ToString();
+                    five_six.Margin = new Thickness(0, 0, 0, 0);
+
+
+                    Point p = e.GetPosition(this);
+
+                    five_six.Margin = new Thickness(p.X, 0, 0, p.Y-1);
+
+                    ALBE.Content += "\n" +five_six.Margin.ToString() +"\n P.X =" + p.X +" P.Y =" + p.Y;
+                   
+
+
+                }
+                }
+            
+            //}
+
+        }
+
         private void five_six_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Point p = e.GetPosition(this);
-                five_six.Margin = new Thickness(p.X, 0, 0, p.Y);
-            }
+            
+            five_six.Margin = new Thickness(120+(kol*100), 0, 0, 550);
+            kol++;
+            
+        }
+
+        private void six_six_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            six_six.Margin = new Thickness(120 + (kol * 100), 0, 0, 550);
+            kol++;
+        }
+
+        private void five_five_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           
+            five_five.Margin = new Thickness(120 + (kol * 100), 0, 0, 550);
+            kol++;
         }
     }
     public class Bone
